0f1d4453da48aedb5feef568abfa6998b9b21391
alexbor <alex.borovik@intel.com>
2017-07-16 15:47:33 +0300

    added old

---
+++ folder1/hellol world.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+chmod ugo+rwx
+
+num = float(input("what is your old: "))
+if num <=10:
+        print("0-10")
+elif num <=20:
+        print("10-20")
+elif num <=30:
+        print("20-30")
+elif num <=40:
+
+        print("30-40")
