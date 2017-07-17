d8ebe3d29d7a9cf6a1665a1a0bca985d4546f703
alexbor <alex.borovik@intel.com>
2017-07-17 11:51:08 +0300

    script added

---
+++ script.py
@@ -0,0 +1,11 @@
+
+at(input("what is your old: "))
+if num <=10:
+        print("0-10")
+elif num <=20:
+        print("10-20")
+elif num <=30:
+        print("20-30")
+elif num <=40:
+        print("30-40")
+
